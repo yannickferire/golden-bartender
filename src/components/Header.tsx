8eb0ff61012b3e004f010a4f3beba47079c58ec0
Yannick Ferire <yannick@bureau347.com>
2024-07-04 00:26:18 +0200

    Add Logo v1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
+
 import { ModeToggle } from "./ModeToggle";
 import { Button } from "./ui/button";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
-    <header className="w-full max-w-screen-xl flex justify-between">
-      <h1 className="font-bold text-md">Golden Bartender</h1>
+    <header className="w-full max-w-screen-xl flex justify-between items-center">
+      <div className="flex space-x-2 items-center">
+        <Logo size={60} />
+        <h1 className="font-bold text-xl leading-5">Golden<br/>Bartender</h1>
+      </div>
       <div className="flex space-x-2">
         <Button variant="linkHover2">Login</Button>
         <Button variant="shine">Sign up</Button>
---
+++ src/components/Logo.tsx
@@ -0,0 +1,31 @@
+interface LogoProps {
+  size: number;
+}
+
+export default function Logo({ size }: LogoProps) {
+  return (
+    <svg id="Calque_2" data-name="Calque 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 238.1 186.99" width={size} height={size}>
+          <g id="Calque_2-2" data-name="Calque 2">
+            <path className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" d="M65.46,37.47s-12.14,8.37-12.42,21.55c-.28,13.18,3.67,74.73-19.2,71.91S-1.46,86.04,8.43,69.94,41.46,5,119.39,5"/>
+            <path className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" d="M172.64,37.47s12.14,8.37,12.42,21.55-3.67,74.73,19.2,71.91c22.87-2.82,35.29-44.89,25.41-60.99C219.79,53.85,196.64,5,118.71,5"/>
+            <g id="Calque_3" data-name="Calque 3">
+              <path className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" d="M73.37,62.88s6.21-14.4,16.38-14.4,4.52,19.53,4.52,19.53"/>
+            </g>
+            <g id="Calque_4" data-name="Calque 4">
+              <g>
+                <path className="fill-secondary" d="M119.39,88.01s0-.05,0-.07c0,.07-.01,.15-.01,.22l.02-.15Z"/>
+                <polygon className="fill-secondary" points="119.37 113.95 119.37 113.96 119.37 113.95 119.37 113.95"/>
+                <path className="fill-secondary" d="M139.14,88.16c0-14.01-19.46-10.03-19.75-.22,0,.02,0,.05,0,.07l-.02,.15c0-.07,.01-.15,.01-.22-.09-9.93-19.76-14.01-19.76,.07,0,17.52,19.76,25.79,19.76,25.79,0,0,0,.06-.02,.15,.05-.02,19.76-8.29,19.76-25.79Z"/>
+              </g>
+              <path className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" d="M165.4,63.03s-6.21-14.4-16.38-14.4-4.52,19.53-4.52,19.53"/>
+              <path className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" d="M162.1,107.55s2.33,28.61-19.16,29.36-23.57-22.96-23.57-22.96c0,0,19.76-8.27,19.76-25.79,0-14.12-19.76-9.98-19.76,0l.02-.15c0-9.98-19.76-14.12-19.76,0,0,17.52,19.76,25.79,19.76,25.79,0,0-2.07,23.72-23.57,22.96s-19.16-29.36-19.16-29.36"/>
+            </g>
+            <g id="Calque_5" data-name="Calque 5">
+              <circle className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" cx="119.39" cy="165.95" r="11.65"/>
+              <path className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" d="M119.39,154.3c-5.04,0-9.33,3.21-10.94,7.7l-23.85-8.61v26.26l23.96-9.43c1.7,4.32,5.9,7.38,10.83,7.38,6.43,0,11.65-5.21,11.65-11.65s-5.21-11.65-11.65-11.65Z"/>
+              <path className="fill-none stroke-zinc-900 stroke-[10px] dark:stroke-[8px] dark:stroke-zinc-100" d="M130.33,162c.45,1.24,.7,2.56,.7,3.95,0,1.51-.3,2.94-.82,4.26l23.96,9.43v-26.26l-23.85,8.61Z"/>
+            </g>
+          </g>
+        </svg>
+  );
+}
